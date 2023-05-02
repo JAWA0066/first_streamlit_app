10fbf092c21dc267bce86374ea3091f76d1d71c7
JAWA0066 <132329354+JAWA0066@users.noreply.github.com>
2023-05-02 12:35:51 +0530

    Update streamlit_app.py
    added code to pick furit by them self

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,26 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 import pandas
-my_furit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.dataframe(my_furit_list)
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#setting fruits as index
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+if fruits_selected:
+
+  fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+else:
+
+    fruits_to_show = my_fruit_list
+
+# Display the table on the page.
+
+streamlit.dataframe(fruits_to_show)
