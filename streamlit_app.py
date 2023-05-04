c6e762340195ce477efa842d2d05ba3bcf65f167
JAWA0066 <132329354+JAWA0066@users.noreply.github.com>
2023-05-04 14:51:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,10 +16,8 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-#setting fruits as index
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Watermelon'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
@@ -28,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 def get_frityvice_data(this_fruit_choice):
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       return fruityvice_normalized
 
@@ -58,7 +56,7 @@ if streamlit.button('Get fruit Load List'):
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"')")
-    return "Thanks for adding "+ new_fruit
+    return "Thanks for adding " + new_fruit
 app_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
